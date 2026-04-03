4ef0c7f5b3e11609a14d448498a45dbc7076d7cd
kevinkiklee <kevin.kik.lee@gmail.com>
2026-04-03 10:17:05 -0400

    fix(exposure): flip WebGL textures to correct upside-down images

--- components/tools/exposure-simulator/useExposureRenderer.ts
+++ components/tools/exposure-simulator/useExposureRenderer.ts
@@ -82,6 +82,7 @@ function loadImageAsTexture(gl: WebGL2RenderingContext, src: string): Promise<{ 
     img.onload = () => {
       const texture = gl.createTexture()!
       gl.bindTexture(gl.TEXTURE_2D, texture)
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, gl.RGBA, gl.UNSIGNED_BYTE, img)
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
